9e59784e5c332e07ad416947d459759e43230f5c
FAYEMI BOLUWATIFE <67077455+BOVAGE@users.noreply.github.com>
2020-11-25 09:46:28 -1200

    naming convention pep8

--- guiweather.py
+++ guiweather.py
@@ -3,71 +3,71 @@ from PIL import ImageTk, Image
 import requests, json, pprint
 
 
-baseWidth = 435
-baseHeight = 400
+base_width = 435
+base_height = 400
 root = tk.Tk()
 root.title("Weather App")
 root.iconbitmap(r"images\logo.ico")
-root.geometry(f"{baseWidth}x{baseWidth}+0+0")
+root.geometry(f"{base_width}x{base_height}+0+0")
 
-def getWeather(cityName: str) -> dict:
+def get_weather(city_name: str) -> dict:
     """ makes a request to openweather api"""
     #open weather api key
-    apiKey = "e75cab1956638b0ff65b76430f0b708e"
-    apiUrl = "https://api.openweathermap.org/data/2.5/weather"
-    params = {"appid": apiKey, "q": cityName, "units": "metric"}
-    response = requests.get(apiUrl, params = params).text
-    weatherDict = json.loads(response)
-    #pprint.pprint(weatherDict)
-    return weatherDict
+    api_key = "e75cab1956638b0ff65b76430f0b708e"
+    api_url = "https://api.openweathermap.org/data/2.5/weather"
+    params = {"appid": api_key, "q": city_name, "units": "metric"}
+    response = requests.get(api_url, params = params).text
+    weather_dict = json.loads(response)
+    #pprint.pprint(weather_dict)
+    return weather_dict
 
-def displayWeather(*events):
+def display_weather(*events):
     """ displays the result to label widgets """
     try:
-        weatherData = getWeather(locationEntry.get())
-        #print(weatherData)
+        weather_data = get_weather(location_entry.get())
+        #print(weather_data)
     except requests.ConnectionError:
         result.set("Check your internet connection")    
     else:
         try:
-            name = weatherData["name"]
-            temp = weatherData["main"]["temp"]
-            country = weatherData["sys"]["country"]
-            description = weatherData["weather"][0]["description"].capitalize()
-            iconid = weatherData["weather"][0]["icon"]
-            iconUrl = f"http://openweathermap.org/img/wn/{iconid}@2x.png"
+            name = weather_data["name"]
+            temp = weather_data["main"]["temp"]
+            country = weather_data["sys"]["country"]
+            description = weather_data["weather"][0]["description"].capitalize()
+            iconid = weather_data["weather"][0]["icon"]
+            icon_url = f"http://openweathermap.org/img/wn/{iconid}@2x.png"
             #get image for the icons
-            #icon = requests.get(iconUrl)
+            #icon = requests.get(icon_url)
             #icon = tk.PhotoImage(file = icon.content)
             result.set(f"City: {name}\nCondition: {description}\nCountry: {country}\nTemperature(\N{DEGREE SIGN}C): {temp}")
         except KeyError:
-            result.set(weatherData.get("message"))
+            result.set(weather_data.get("message"))
     
 
-backgroundImage = tk.PhotoImage(file = "images/background.png")
-backgroundLabel = tk.Label(root, image = backgroundImage)
-backgroundLabel.place(relwidth = 1.0, relheight = 1.0)
+background_image = tk.PhotoImage(file = "images/background.png")
+background_label = tk.Label(root, image = background_image)
+background_label.place(relwidth = 1.0, relheight = 1.0)
 
 #frame for entry and button widgets
-topFrame = tk.Frame(root, bg = "skyblue", bd = "3")
-topFrame.place(relx = 0.1, rely = 0.1, relwidth = 0.8, relheight = 0.2)
+top_frame = tk.Frame(root, bg = "skyblue", bd = "3")
+top_frame.place(relx = 0.1, rely = 0.1, relwidth = 0.8, relheight = 0.2)
 
 #frame for text widget
-bottomFrame = tk.Frame(root, bg = "skyblue", bd = "5")
-bottomFrame.place(relx = 0.1, rely = 0.35, relwidth = 0.8, relheight = 0.6)
+bottom_frame = tk.Frame(root, bg = "skyblue", bd = "5")
+bottom_frame.place(relx = 0.1, rely = 0.35, relwidth = 0.8, relheight = 0.6)
 
-locationEntry = tk.Entry(topFrame, fg = "black", font = ("Arial", 18, "bold"))
-locationEntry.place(relwidth = 0.7, relheight = 0.9, rely = 0.05)
+location_entry = tk.Entry(top_frame, fg = "black", font = ("Arial", 18, "bold"))
+location_entry.place(relwidth = 0.7, relheight = 0.9, rely = 0.05)
 
-getWeatherBtn = tk.Button(topFrame, text = "Get Weather", bg = "blue", bd = "4",fg = "#000d33",
+get_weatherBtn = tk.Button(top_frame, text = "Get Weather", bg = "blue", bd = "4",fg = "#000d33",
                           relief = "raised", font = ("Arial", 12),
-                          command = displayWeather)
-getWeatherBtn.place(relx = 0.71, rely = 0.05, relwidth = 0.29, relheight = 0.9)
+                          command = display_weather)
+get_weatherBtn.place(relx = 0.71, rely = 0.05, relwidth = 0.29, relheight = 0.9)
 
-root.bind("<Return>", displayWeather)
+root.bind("<Return>", display_weather)
 
 result = tk.StringVar()
-resultText = tk.Label(bottomFrame, textvariable = result, font = ("Arial", 16))
-resultText.place(relwidth = 1.0, relheight = 1.0)
+result_text = tk.Label(bottom_frame, textvariable = result, font = ("Arial", 16))
+result_text.place(relwidth = 1.0, relheight = 1.0)
 
 root.mainloop()
